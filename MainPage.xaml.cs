4f107c151a8b76fc94151e02a5e4fc32a8a8fb51
Kamva9697 <kamva9697@gmail.com>
2024-02-08 17:40:15 +0200

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using ProfilePage.ViewModels;
+
+namespace ProfilePage
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new ProfileViewModel();
+        }
+    }
+
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ProfilePage
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
